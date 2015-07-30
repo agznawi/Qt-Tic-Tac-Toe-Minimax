8dd1568c47b7588f9900336dfb0c04d27a99a549
AGhaznawi <abdullahalghaznawy@yahoo.com>
2015-07-31 01:21:38 +0200

    Update Ai.h

--- TicTacToe/Ai.h
+++ TicTacToe/Ai.h
@@ -7,7 +7,7 @@
 class Ai
 {
 private:
-    enum { WIN_SCORE = 100, TIE_SCORE = 0, INF = 100000 };
+    enum { WIN_SCORE = 100, TIE_SCORE = 0, INF = 999999 };
 
 public:
     Move Search(int player);
